6ae6673c1df962726f029b028d07d258d135f5aa
Karolis <karolisburtazodis@gmail.com>
2023-04-17 16:20:24 +0300

    feat: Add Home & Login pages, Router Component.

---
+++ app-fe/views/Flashcards.vue
@@ -0,0 +1,124 @@
+<script setup>
+  import { ref } from "vue";
+</script>
+<template>
+ <div class="ag-format-container">
+  <div class="ag-courses_box"><ul><h2 v-for="(Flashcards, i) in Flashcards" :key="i">
+    <div class="ag-courses_item">
+      <a href="#" class="ag-courses-item_link">
+        <div class="ag-courses-item_bg">
+        </div>
+        <div class="ag-courses-item_title">
+     {{ Flashcards }}
+        </div>
+      </a>
+    </div>
+ </h2></ul></div>
+</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      Flashcards: ['History', 'Eng','0'],
+    };
+  },
+};
+</script>
+<style>
+.ag-format-container {
+  width: 1142px;
+  margin: 0 auto;
+  
+}
+
+template {
+  background-color: #000;
+}
+.ag-courses_box {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: start;
+  -ms-flex-align: start;
+  align-items: flex-start;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  justify-content: center;
+  text-align: center;
+  padding: 30px 0;
+}
+.ag-courses_item {
+  -ms-flex-preferred-size: calc(33.33333% - 30px);
+  flex-basis: calc(33.33333% - 30px);
+
+  margin: 0 95px 45px;
+  overflow: hidden;
+
+  border-radius: 28px;
+}
+.ag-courses-item_link {
+  display: block;
+  padding: 45px 400px;
+  background-color: #121212;
+
+  overflow: hidden;
+  text-align: center;
+  position: relative;
+}
+.ag-courses-item_link:hover,
+.ag-courses-item_link:hover .ag-courses-item_date {
+  text-decoration: none;
+  color: #FFF;
+}
+.ag-courses-item_link:hover .ag-courses-item_bg {
+  -webkit-transform: scale(10);
+  -ms-transform: scale(10);
+  transform: scale(10);
+}
+.ag-courses-item_title {
+  min-height: 87px;
+  margin: 0 0 25px;
+  overflow: hidden;
+  font-weight: bold;
+  font-size: 30px;
+  color: #FFF;
+
+  z-index: 2;
+  position: relative;
+}
+.ag-courses-item_date-box {
+  font-size: 18px;
+  color: #FFF;
+
+  z-index: 2;
+  position: relative;
+}
+.ag-courses-item_date {
+  font-weight: bold;
+  color: #f9b234;
+
+  -webkit-transition: color .5s ease;
+  -o-transition: color .5s ease;
+  transition: color .5s ease
+}
+.ag-courses-item_bg {
+  height: 128px;
+  width: 128px;
+  background-color: #f9b234;
+
+  z-index: 1;
+  position: absolute;
+  top: -75px;
+  right: -75px;
+
+  border-radius: 50%;
+
+  -webkit-transition: all .5s ease;
+  -o-transition: all .5s ease;
+  transition: all .5s ease;
+}
+.ag-courses_item:nth-child(6n) .ag-courses-item_bg {
+  background-color: #4c49ea;
+}
+</style>
---
+++ app-fe/views/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="about-container">
+    <section class="vh-100">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="https://images.unsplash.com/photo-1554252117-53f26a5ebdbd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+          class="img-fluid" alt="Image">
+      </div>
+      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <form>
+          <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+            <p class="lead fw-normal mb-0 me-3">Login to use Flashcards App!</p>
+          </div>
+
+          <div class="divider d-flex align-items-center my-4">
+          </div>
+
+          <div class="form-outline mb-4">
+            <input type="email" id="email" class="form-control form-control-lg"
+              placeholder="Enter a valid email address" required/>
+            <label class="form-label" for="email">Email address</label>
+          </div>
+
+          <div class="form-outline mb-3">
+            <input type="password" id="password" class="form-control form-control-lg"
+              placeholder="Enter password" required />
+            <label class="form-label" for="password">Password</label>
+          </div>
+
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="form-check mb-0">
+              <input class="form-check-input me-2" type="checkbox" value="" id="remember" />
+              <label class="form-check-label" for="remember">
+                Remember me
+              </label>
+            </div>
+          </div>
+
+          <div class="text-center text-lg-start mt-4 pt-2">
+            <button type="button" class="btn btn-primary btn-lg"
+              style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? 
+                <router-link class="link-danger" :to="{name: 'Register'}">Take me to the Register page!</router-link>
+            </p>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+  </div>
+</template>
+
+<!-- Don't use any styles, look  for bootstrap classes -->
+<style>
+.divider:after,
+.divider:before {
+content: "";
+flex: 1;
+height: 1px;
+background: #eee;
+}
+.h-custom {
+height: calc(100% - 73px);
+}
+</style>
